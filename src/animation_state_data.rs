86195c8429999a23067bce76553525ae16b09759
Aloento <11802769+soarteam@users.noreply.github.com>
2021-02-21 14:05:11 +0100

    AnimationStateData

--- src/animation_state_data.rs
+++ src/animation_state_data.rs
@@ -1,5 +1,35 @@
 use crate::skeleton_data::SkeletonData;
+use std::collections::HashMap;
+use crate::animation::Animation;
 
 pub struct AnimationStateData<'a> {
     skeletonData: &'a SkeletonData<'a>,
+    animationToMixTime: HashMap<Key<'a>, f32>,
+    tempKey: Key<'a>,
+    defaultMix: f32,
+}
+
+impl<'a> AnimationStateData<'a> {
+    pub fn new(skeletonData: &'a SkeletonData<'a>) -> Self {
+        Self {
+            skeletonData,
+            animationToMixTime: Default::default(),
+            tempKey: Key::new(),
+            defaultMix: 0.0
+        }
+    }
+}
+
+struct Key<'a> {
+    a1: Option<&'a Animation>,
+    a2: Option<&'a Animation>,
+}
+
+impl<'a> Key<'a> {
+    pub fn new() -> Self {
+        Self {
+            a1: None,
+            a2: None
+        }
+    }
 }
