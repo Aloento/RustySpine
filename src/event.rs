ec0d6cbef79d4d25b056ce4d98cc396f90940e37
Aloento <11802769+soarteam@users.noreply.github.com>
2021-02-21 14:16:09 +0100

    Event

--- src/event.rs
+++ src/event.rs
@@ -1,1 +1,25 @@
-pub struct Event {}
+use crate::event_data::EvenData;
+
+pub struct Event<'a> {
+    time: f32,
+    data: &'a EvenData,
+    intValue: i32,
+    floatValue: f32,
+    stringValue: String,
+    volume: f32,
+    balance: f32,
+}
+
+impl<'a> Event<'a> {
+    pub fn new(time: f32, data: &'a EvenData) -> Self {
+        Self {
+            time,
+            data,
+            intValue: 0,
+            floatValue: 0.0,
+            stringValue: "".to_string(),
+            volume: 0.0,
+            balance: 0.0
+        }
+    }
+}
